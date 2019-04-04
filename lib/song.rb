7993abc40aa3d33ec71436d5bb393894783575a4
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-04 19:27:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,11 +40,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)
-    if song.name == name
-      return song
-    else
-      self.create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
       return song
     end
   end
